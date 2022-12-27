2e797f26d017eb99dbc88a3353f0a8da8e83c432
mvatanasov18 <54852701+mvatanasov18@users.noreply.github.com>
2022-12-27 11:01:06 +0200

    Add tags component

---
+++ src/Tags.js
@@ -0,0 +1,8 @@
+export const Tags = (props) => {
+    return (
+        <ul className="tags">
+            {props.tags.map((t, index) => {
+                 return <li key={index} className="tag">{'#' + ' ' + t}</li>})}
+        </ul>
+    )
+}
